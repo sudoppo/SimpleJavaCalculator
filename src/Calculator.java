d3add774d7808b19c2e1612073236419b5482376
Vasco Vicente <141693577+VascoVicente12@users.noreply.github.com>
2024-08-20 14:11:41 +0100

    Created Base Java Project

---
+++ src/Calculator.java
@@ -0,0 +1,20 @@
+public class Calculator {
+
+    double currentValue;
+    double previousValue;
+    String currentOperation;
+
+    public Calculator() {
+
+    }
+
+    public void inputValue(double number){}
+
+    public void inputOperation(String operation){}
+
+    public double calculate(){
+        return 0;
+    }
+
+    public void clear(){}
+}
---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        JFrame frame = new JFrame("Simple Application");
+
+
+        frame.setSize(1200, 1200);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+
+        frame.setVisible(true);
+    }
+}
